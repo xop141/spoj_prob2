8f8c8f38e914498441f3a78ee776737b76337fd3
xop141 <lowsens400@gmail.com>
2024-12-03 13:07:59 -0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,161 @@
+//let davhar = 3, ail = 3,orts = 3,toot = 26
+//let orts1 = Math.ceil(toot/(ail*davhar))
+//console.log(orts1,"orts");
+//let x = toot-(orts1-1)*davhar*ail
+//console.log(Math.ceil(x/ail),"davhar");
+//if (x%ail == 0) {
+//    console.log("3");
+//} else{
+//    console.log(x%ail);
+//}
+
+
+
+//let a = 10
+//console.log((1+a)/2*a);
+
+//let a = 2
+//console.log((a*(a+1)*(2*a+1)/6)
+//);
+
+//let a = 5
+//console.log((2+(((a+1)/2)-1)*2)/2*((a+1)/2));
+
+//let a = 7200
+//while (a>=10) {
+//    a = a/10
+//}
+//console.log(Math.floor(a));
+
+
+
+
+
+
+//*************/ Цифрүүдийн нийлбэр
+//let a = 1258
+//const b = a
+//let oron = 0, x = 0
+//while (a>1) {
+//   a = a/10
+//    oron = oron + 1
+//}
+//let oron2 = oron
+//while (oron2>0) {
+//   x = x + Math.floor((b%Math.pow(10, oron2))/Math.pow(10, oron2-1))
+//  oron2 = oron2 - 1
+//} 
+//console.log(x);
+
+
+
+//let a = 254
+//const b = a
+//let sda = 0
+//let oron = 0, x = 0
+//while (a>1) {
+//   a = a/10
+//  oron = oron + 1
+//}
+//let oron2 = oron
+//while (oron2>0) {
+//x = Math.floor((b%Math.pow(10, oron2))/Math.pow(10, oron2-1))
+//if (Math.ceil(x/2)==(x/2)) {
+//    sda = sda + x   
+//} 
+//oron2 = oron2 - 1
+//} 
+//console.log(sda);
+
+
+//let a = 1250, sda = 0
+//const b = a
+//let oron = 0, x = 0
+//while (a>=1) {
+//   a = a/10
+//    oron = oron + 1
+//}
+//let oron2 = oron
+//while (oron2>0) {
+//   x =  Math.floor((b%Math.pow(10, oron2))/Math.pow(10, oron2-1))
+//   if (Math.floor(x/2)<(x/2)) {
+//        sda = sda + 1   
+//    } 
+//  oron2 = oron2 - 1
+//} 
+//console.log(sda);
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
