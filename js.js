307d8cf6f40192a0dc60c5287433e3215d8bfc46
xop141 <lowsens400@gmail.com>
2024-12-04 09:52:20 -0800

    Update js.js

--- js.js
+++ js.js
@@ -68,23 +68,23 @@
 //console.log(sda);
 
 
-//let a = 1250, sda = 0
+//let a = 123477
 //const b = a
+//let sda = 0
 //let oron = 0, x = 0
-//while (a>=1) {
+//while (a>1) {
 //   a = a/10
-//    oron = oron + 1
+//  oron = oron + 1
 //}
 //let oron2 = oron
 //while (oron2>0) {
-//   x =  Math.floor((b%Math.pow(10, oron2))/Math.pow(10, oron2-1))
-//   if (Math.floor(x/2)<(x/2)) {
-//        sda = sda + 1   
-//    } 
-//  oron2 = oron2 - 1
+//x = Math.floor((b%Math.pow(10, oron2))/Math.pow(10, oron2-1))
+//if (x%2==1) {
+//    sda = sda + x   
+//} 
+//oron2 = oron2 - 1
 //} 
 //console.log(sda);
-
 
 
     
